668705c04eceb975f9c106a0c1a322b17c5352a5
Handzsúr István <45454828+trjoin@users.noreply.github.com>
2019-04-10 09:27:36 +0200

    Update szerkeszt.php

--- wp-admin/szerkeszt.php
+++ wp-admin/szerkeszt.php
@@ -20,15 +20,18 @@ if(isset($_GET["modosit"]))
 	echo "<p align='left'><big><b><u>Meta KEYWORDS:</u></b></big><br><input type='text' name='metakeywords' value='".$oldal["metakeywords"]."'></p>";
 	echo "<p align='left'><big><b><u>Meta DESCRIPTION:</u></b></big><br><input type='text' name='metadesc' value='".$oldal["metadesc"]."'></p>";
 	echo "<p align='left'><big><b><u>Külső hivatkozás (ha van):</u></b></big><br><input type='text' name='tolink' value='".$oldal["tolink"]."'></p>";
-	echo "<p align='left'><big><b><u>Modul hozzárendelése (ha telepítve van):</u></b></big><br>";
-	echo "<select name='tomodul'>
-		<option value='".$oldal["tomodul"]."' selected>".$oldal["tomodul"]."</option";
-		$bt1=$pdo->query("select * from ".$elotag."_modulok where bekapcsolva='igen' and integ='1'");
-		while($md1=$bt1->fetch())
-		{
-			echo '<option value="'.$md1["modulnev"].'">'.$md1["modulnev"].'</option>';
-		}
-	echo "</select></p><br /><br />";
+	$bt1=$pdo->query("select * from ".$elotag."_modulok where bekapcsolva='igen' and integ='1'");
+	if($bt1->rowCount()>0)
+	{
+		echo "<p align='left'><big><b><u>Modul hozzárendelése (ha telepítve van):</u></b></big><br>";
+		echo "<select name='tomodul'>
+			<option value='".$oldal["tomodul"]."' selected>".$oldal["tomodul"]."</option";
+			while($md1=$bt1->fetch())
+			{
+				echo '<option value="'.$md1["modulnev"].'">'.$md1["modulnev"].'</option>';
+			}
+		echo "</select></p><br /><br />";
+	}
 }
 if(isset($_GET["almodosit"]))
 {
@@ -39,15 +42,18 @@ if(isset($_GET["almodosit"]))
 	echo "<p align='left'><big><b><u>Meta KEYWORDS:</u></b></big><br><input type='text' name='metakeywords' value='".$oldal["metakeywords"]."'></p>";
 	echo "<p align='left'><big><b><u>Meta DESCRIPTION:</u></b></big><br><input type='text' name='metadesc' value='".$oldal["metadesc"]."'></p>";
 	echo "<p align='left'><big><b><u>Külső hivatkozás (ha van):</u></b></big><br><input type='text' name='tolink' value='".$oldal["tolink"]."'></p>";
-	echo "<p align='left'><big><b><u>Modul hozzárendelése (ha telepítve van):</u></b></big><br>";
-	echo "<select name='tomodul'>
-		<option value='".$oldal["tomodul"]."' selected>".$oldal["tomodul"]."</option";
-		$bt1=$pdo->query("select * from ".$elotag."_modulok where bekapcsolva='igen' and integ='1'");
-		while($md1=$bt1->fetch())
-		{
-			echo '<option value="'.$md1["modulnev"].'">'.$md1["modulnev"].'</option>';
-		}
-	echo "</select></p><br /><br />";
+	$bt1=$pdo->query("select * from ".$elotag."_modulok where bekapcsolva='igen' and integ='1'");
+	if($bt1->rowCount()>0)
+	{
+		echo "<p align='left'><big><b><u>Modul hozzárendelése (ha telepítve van):</u></b></big><br>";
+		echo "<select name='tomodul'>
+			<option value='".$oldal["tomodul"]."' selected>".$oldal["tomodul"]."</option";
+			while($md1=$bt1->fetch())
+			{
+				echo '<option value="'.$md1["modulnev"].'">'.$md1["modulnev"].'</option>';
+			}
+		echo "</select></p><br /><br />";
+	}
 }
 if(isset($_GET["blokkszerk"]))
 {
